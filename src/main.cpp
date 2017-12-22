2580dd95ad8b43b249ea083a80d665d694774fe1
lennard <lennard-boediger@t-online.de>
2017-12-22 14:15:31 +0100

    fixed init

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,7 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< HEAD
+
     // psdfsdfihaoefhut your main code here, to run repeatedly:
-=======
-    // psdfsdfut your main code here, to run repeatedly:
->>>>>>> 55a60d56070ea70ea6b6eb8c95e6a6bf35103bb8
+
 }
